ad413d6fd828cd2c8289472ac911cb119ecd180a
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-06-27 17:28:36 +0530

    fix

--- infra/main.tf
+++ infra/main.tf
@@ -144,7 +144,7 @@ module "lambda_search" {
   source          = "../modules/lambda"
   function_name   = var.search_lambda_name
   s3_bucket       = var.lambda_code_bucket
-  s3_key          = var.search_lambda_key
+  # s3_key          = var.search_lambda_key
   # handler         = var.search_lambda_handler
   runtime         = var.lambda_runtime
   role_arn        = module.lambda_iam_role.iam_role_arn
